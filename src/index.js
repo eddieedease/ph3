1134f6e7d954be93e0606c4390d41023a2cb0676
eddieedease <eddie@edease.nl>
2018-07-18 21:07:16 +0200

    tilemapandswitching

--- src/index.js
+++ src/index.js
@@ -9,27 +9,78 @@ var Game1 = new Phaser.Class({
 
     initialize:
 
-    function Game1 ()
-    {
-        Phaser.Scene.call(this, { key: 'game1', active: true });
-    },
+        function Game1() {
+            Phaser.Scene.call(this, {
+                key: 'game1',
+                active: true
+            });
+        },
 
-    preload: function ()
-    {
+    preload: function () {
         //this.load.image('picA', 'assets/pics/lance-overdose-loader-eye.png');
+        // tilemap json
+        this.load.image('tiles', 'assets/tilemaps/tileset.png');
+
+        this.load.tilemapTiledJSON({
+            key: 'tileset',
+            url: 'assets/tilemaps/tileset.json'
+        });
     },
 
-    create: function ()
-    {
-        // Mouseclick to start
-        this.input.once('pointerup', function (event) {
+    create: function () {
+        // INPUT KEYBOARD SWITCH GAME
+        this.input.keyboard.on('keyup_ONE', function (event) {
 
             console.log('Starting');
 
-            this.scene.start('game2');
+            this.scene.switch('game2');
 
         }, this);
+
+
+        // loading the json tilemap
+        var map = this.make.tilemap({
+            key: 'tileset'
+        });
+
+        var tiles = map.addTilesetImage('tileset', 'tiles');
+
+        var layer1 = map.createStaticLayer(0, tiles, 0, 0);
+        var layer2 = map.createStaticLayer(1, tiles, 0, 0);
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        var controlConfig = {
+            camera: this.cameras.main,
+            left: this.cursors.left,
+            right: this.cursors.right,
+            up: this.cursors.up,
+            down: this.cursors.down,
+            speed: 0.5
+        };
+
+        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+
+        this.input.on('pointerdown', function () {
+
+            this.cameras.main.shake(100);
+    
+        }, this);
+
+        this.helpText = this.add.text(16, 16, 'Press 1 | 2', {
+            fontSize: '18px',
+            fill: '#ffffff'
+        });
+        this.helpText.setScrollFactor(0);
+
     }
+    ,
+    update: function (time, delta) {
+        this.controls.update(delta);
+    }
+
 
 });
 
@@ -40,49 +91,52 @@ var Game2 = new Phaser.Class({
 
     initialize:
 
-    function Game2 ()
-    {
-         Phaser.Scene.call(this, { key: 'game2', active: false });
-    },
+        function Game2() {
+            Phaser.Scene.call(this, {
+                key: 'game2',
+                active: false
+            });
+        },
 
-    preload: function ()
-    {
+    preload: function () {
         this.load.image('bg', 'assets/bg.png');
-    this.load.image('johnny', 'assets/johnny.png');
-    this.load.image('bubble', 'assets/bubble.png');
-    // load our atlasfile
-    this.load.atlas('ss', 'assets/spritesheets/ss.png', 'assets/spritesheets/ss.json');
+        this.load.image('johnny', 'assets/johnny.png');
+        this.load.image('bubble', 'assets/bubble.png');
+        // load our atlasfile
+        this.load.atlas('ss', 'assets/spritesheets/ss.png', 'assets/spritesheets/ss.json');
     },
 
-    create: function ()
-    {
+    create: function () {
+
+
+
         console.log(game);
         this.bg = this.add.image(0, 0, 'bg').setOrigin(0);
         //  Set the camera and physics bounds to be the size of 4x4 bg images
         this.cameras.main.setBounds(0, 0, 1230, 700);
         this.cameras.main.setSize(900, 700);
         this.physics.world.setBounds(0, 0, 1230, 700);
-    
-    
+
+
         //  This frame is in the 1st atlas file (set0/data0)
-    
-    
+
+
         this.cursors = this.input.keyboard.createCursorKeys();
-    
+
         this.add.image(1000, config.height - 230, 'johnny');
         var thisbubble = this.add.image(1000, config.height - 590, 'bubble').setAlpha(0.0);;
-    
+
         this.tweens.add({
             targets: thisbubble,
-            y : config.height - 390,
+            y: config.height - 390,
             alpha: 1,
             duration: 400,
             ease: 'back',
-           //  yoyo: true,
+            //  yoyo: true,
             // delay: 1000,
             // repeat: 2 // set -1 for infinite
         });
-    
+
         var textureFrames = this.textures.get('ss').getFrameNames();
         var animFrames = [];
         animFrames.push({
@@ -94,45 +148,58 @@ var Game2 = new Phaser.Class({
             frames: animFrames,
             repeat: -1
         });
-    
+
         // all the frames for running
         this.anims.create({
             key: 'run',
             frames: this.anims.generateFrameNames('ss'),
             repeat: -1
         });
-    
-    
+
+
         // // future REFS
         // var config = {
         //     key: 'explode',
         //     frames: this.anims.generateFrameNumbers('boom', { frames: [ 0, 1, 2, 1, 2, 3, 4, 0, 1, 2 ] }),
         //     frameRate: 20
         // };
-    
+
         this.player = this.physics.add.sprite(50, config.height - 160, 'ss').play('idle').setScale(0.5);
-    
-    
+
+
         this.player.setCollideWorldBounds(true);
-    
+
         this.cameras.main.startFollow(this.player);
-    
+
         // this.cameras.main.followOffset.set(-300, 0);
-    
+
         this.input.keyboard.on('keydown', function (event) {
-    
+
             console.log(event.key);
-    
+
         });
+
+        // Mouseclick to start other event
+        this.input.keyboard.on('keyup_TWO', function (event) {
+
+            console.log('Starting');
+            this.scene.switch('game1');
+        }, this);
+
+
+        this.helpText = this.add.text(16, 16, 'Press 1 | 2', {
+            fontSize: '18px',
+            fill: '#ffffff'
+        });
+        this.helpText.setScrollFactor(0);
     },
-    update: function ()
-    {
+    update: function () {
         //this.pic.rotation -= 0.02;
         this.player.setVelocity(0);
 
         var _newstate;
-    
-    
+
+
         if (this.cursors.left.isDown) {
             _newstate = 'moving';
             this.player.setVelocityX(-300);
@@ -148,33 +215,40 @@ var Game2 = new Phaser.Class({
         }
         this.update_checkPlayerAnimation(_newstate);
         // Endless movement
-        if (this.player.x < 40 ){
+        if (this.player.x < 40) {
             console.log('outofboundsleft');
             this.player.x = 1200;
-        } 
-        
-        if (this.player.x > 1200){
+        }
+
+        if (this.player.x > 1200) {
             this.player.x = 40;
         }
     },
 
     update_checkPlayerAnimation(_newstate) {
-        
+
         // lets check if the animation needs to be changed
         if (this.player_animstate !== _newstate) {
             this.player_animstate = _newstate;
             switch (_newstate) {
                 case 'moving':
-                this.player.play('run');
+                    this.player.play('run');
                     break;
                 case 'idle':
-                this.player.play('idle');
+                    this.player.play('idle');
                     break;
-    
+
             }
         } else {
             // Do nothing and keep animation going
         }
+    }
+
+    ,
+
+    shutdown: function () {
+        //  We need to clear keyboard events, or they'll stack up when the Menu is re-run
+        this.input.keyboard.shutdown();
     }
 
 
@@ -191,23 +265,10 @@ var config = {
     physics: {
         default: 'arcade',
     },
-    scene: [Game1,Game2]
+    scene: [Game1, Game2]
 };
 
 
 
 
 var game = new Phaser.Game(config);
-
-
-
-
-
-
-
-
-
-
-
-
-
